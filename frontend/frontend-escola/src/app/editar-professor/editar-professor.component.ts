5b7e024b6362c4f4fbf7d6b58286bc0a64ea2b6b
Daniel <danielcarmo29@gmail.com>
2024-11-21 15:03:53 -0300

    crud finalizado e testado

--- frontend/frontend-escola/src/app/editar-professor/editar-professor.component.ts
+++ frontend/frontend-escola/src/app/editar-professor/editar-professor.component.ts
@@ -10,6 +10,7 @@ import { ProfessorService } from './services/professor.service';
 export class EditarProfessorComponent implements OnInit {
   idProfessor?: number;
   professor: any = {};
+  cpfValido: boolean = true;
 
   constructor(
     private professorService: ProfessorService,
@@ -33,6 +34,11 @@ export class EditarProfessorComponent implements OnInit {
       alert('ID do professor não encontrado.');
       return;
     }
+    if (!this.validarCPF(this.professor.cpf)) {
+      this.cpfValido = false; // Indica que o CPF está inválido
+      alert('CPF inválido. Verifique o valor e tente novamente.');
+      return;
+    }
 
     this.professor.id = this.idProfessor;
 
@@ -46,5 +52,38 @@ export class EditarProfessorComponent implements OnInit {
         alert('Erro ao editar professor: ' + error.message);
       }
     );
+  }
+  validarCPF(cpf: string): boolean {
+    const regex = /^\d{3}\.\d{3}\.\d{3}-\d{2}$/;
+    if (!regex.test(cpf)) {
+      return false;
+    }
+
+    const cpfLimpo = cpf.replace(/[^\d]+/g, '');
+    if (cpfLimpo.length !== 11 || /^(\d)\1{10}$/.test(cpfLimpo)) {
+      return false;
+    }
+
+    let soma = 0;
+    let resto;
+
+    for (let i = 1; i <= 9; i++) {
+      soma += parseInt(cpfLimpo.substring(i - 1, i)) * (11 - i);
+    }
+
+    resto = (soma * 10) % 11;
+    if (resto === 10 || resto === 11) resto = 0;
+    if (resto !== parseInt(cpfLimpo.substring(9, 10))) return false;
+
+    soma = 0;
+    for (let i = 1; i <= 10; i++) {
+      soma += parseInt(cpfLimpo.substring(i - 1, i)) * (12 - i);
+    }
+
+    resto = (soma * 10) % 11;
+    if (resto === 10 || resto === 11) resto = 0;
+    if (resto !== parseInt(cpfLimpo.substring(10, 11))) return false;
+
+    return true;
   }
 }
